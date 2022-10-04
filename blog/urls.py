f0563d61cfaec8ced756bd95ff3de7eadb3a2fbf
Daniel <daniel.rosofsky.ryan@gmail.com>
2022-10-04 13:15:39 -0600

    Chapter 7

--- blog/urls.py
+++ blog/urls.py
@@ -1,5 +1,11 @@
 from django.urls import path
-from .views import *
+from .views import (
+    BlogListView,
+    BlogDetailView,
+    BlogCreateView,
+    BlogUpdateView,
+    BlogDeleteView,
+)
 
 urlpatterns = [
     path("post/new/", BlogCreateView.as_view(), name="post_new"),
